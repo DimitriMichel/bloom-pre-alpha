966324b9d46b9fcbd1bd2450f6416b7cf469332e
Dimitri Pierre-Louis <dimitrimachines@gmail.com>
2022-09-01 13:21:03 -0400

    Removed unused method from controller

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -23,16 +23,6 @@ class GroupController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-     //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -63,17 +53,6 @@ class GroupController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -100,6 +79,6 @@ class GroupController extends Controller
        $group->delete();
        return response()->json([
         'message' => 'Group deleted successfully'
-       ], 200);
+       ], 204);
     }
 }
