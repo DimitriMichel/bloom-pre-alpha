fbcbfc739b859b38fe4ec751b4f2596855a03f58
Dimitri Pierre-Louis <dimitrimachines@gmail.com>
2022-09-21 00:24:32 -0400

    form validation errors

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GroupController;
 use App\Http\Controllers\OrganizationController;
 use App\Http\Controllers\PostController;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use App\Http\Requests\Auth\LoginRequest;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,3 +27,33 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::apiResource('groups', GroupController::class);
 Route::apiResource('organizations', OrganizationController::class);
 Route::apiResource('posts', PostController::class);
+
+Route::post('login', function (LoginRequest $request) {
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || $request->email != $user->email) {
+        throw ValidationException::withMessages([
+            'email' => 'The provided email is does not exist'
+        ]);
+    }
+
+     //if (! $user || ! Hash::check($request->password, $user->password))
+     if ($request->password != $user->password && $request->email === $user->email) {
+        throw ValidationException::withMessages([
+            'password' => 'The provided password is incorrect.'
+        ]);
+    }
+
+    if (!$request->device_name) {
+        throw ValidationException::withMessages([
+            'device_name' => 'The device name is missing from the request'
+        ]);
+    }
+ 
+    $token = $user->createToken($request->device_name)->plainTextToken;
+    return response()->json([
+        'token' => $token,
+        'user' => $user
+    ]);
+});
